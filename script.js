f3200efa57d9078eca80132df1f3cb4a3dd9e981
Fernando Henrique <cara-se@hotmail.com>
2021-11-09 16:16:26 -0300

    Projeto pronto, falta aplicar responsividade

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+function trocarimagem (anything) {
+    document.querySelector('.product').src = anything
+    
+}
+
+function trocarcor (cor) {
+    const extras = document.querySelector('.extras');
+    extras.style.background = cor;
+    
+    
+}
